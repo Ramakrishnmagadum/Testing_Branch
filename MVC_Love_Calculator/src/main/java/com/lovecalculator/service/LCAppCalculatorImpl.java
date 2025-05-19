3d3a556bd55cfb43deb8c602a78b35ce54833de6
Ramakrishna Magadum Kallappa <ramakrishnamagadum.k@auracloud.com>
2025-05-19 15:59:41 +0530

    Modified

--- MVC_Love_Calculator/src/main/java/com/lovecalculator/service/LCAppCalculatorImpl.java
+++ MVC_Love_Calculator/src/main/java/com/lovecalculator/service/LCAppCalculatorImpl.java
@@ -6,6 +6,11 @@ import org.springframework.stereotype.Service;
 public class LCAppCalculatorImpl implements LCAppCalculator {
 
 	public String lovecalculation(String username, String Crushname) {
+String username;
+String name ;
+if(name=="ram"){
+}
+System.out.println(1/0);
 		int size = (username.length() + Crushname.length()) % 5;
 //		FLAME   -->Friend , Love , Affection , Marrige ,Enemy
 		String[] value = { "Friend", "Love", "Affection", "Marrige", "Enemy"};
